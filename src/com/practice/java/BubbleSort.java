d32fdc88e52a41d2ba340eaac4ec3addc0330e1f
cookie-army <cookiearmycia@gmail.com>
2023-09-20 09:49:58 +0530

    updated BubbleSort.java

--- src/com/practice/java/BubbleSort.java
+++ src/com/practice/java/BubbleSort.java
@@ -3,20 +3,17 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1};
+        int[] arr = {10, 6, 3, 4, 7, 1, 8, 5, 9, 2};
         int n = arr.length;
-        for(int i=0;i<n;i++)
-        {
-            for(int j = i+1; j<n; j++)
-            {
-                if(arr[i]>arr[j])
-                {
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (arr[i] > arr[j]){
                     int temp = arr[i];
-                    arr[i]=arr[j];
+                    arr[i] = arr[j];
                     arr[j] = temp;
                 }
             }
         }
-        System.out.println("\nSorted array is"+" "+ Arrays.toString(arr));
+        System.out.print("The sorted array is"+ " " +Arrays.toString(arr));
     }
 }
